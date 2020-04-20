4ad14f816a4ecbb6d24a34a5694ab233e65669d3
Shadowrealism <36948724+Shadowrealism@users.noreply.github.com>
2020-04-20 23:56:34 +0700

    Update topup_proses.php

--- topup_proses.php
+++ topup_proses.php
@@ -2,18 +2,25 @@
 session_start();
 $dsn = "mysql:host=localhost;dbname=proyek";
 $kunci = new PDO($dsn, "root", "");
-try {
-    // add to the receiving account
-    $sql = 'UPDATE account
-                      SET balance = balance + ?
-                      WHERE id = ?';
-    $stmt = $kunci->prepare($sql);
-    $data = [$_POST['Balance'], $_POST['id']];
-    $stmt->execute($data);
-    // commit the transaction
 
-    echo 'The balance has been transferred successfully';
-  } catch (PDOException $e) {
-      die($e->getMessage());
-}
-?>
+        try {
+
+            // get available balance of the transferer account
+            $sql = 'SELECT balance FROM account WHERE id= ?';
+            $stmt = $kunci->prepare($sql);
+            $data = $_POST['id']
+            $stmt-> execute($stmt);
+            $data = $stmt-> fetch();
+
+            // add to the receiving account
+            $sql_update_to = 'UPDATE account
+                                SET balance = balance + :balance
+                                WHERE id = :to';
+            $stmt = $kunci->prepare($sql_update_to);
+            $stmt->execute(array(":to" => $_POST['id'], ":balance" => $data['Balance']));
+            // commit the transaction
+
+            echo 'The balance has been transferred successfully';
+        } catch (PDOException $e) {
+            die($e->getMessage());
+        }
